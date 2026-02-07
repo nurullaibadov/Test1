95a4fe4933cfd1c6a420eeecf011c78defe039cc
Nurulla Ibadov <ibadnurulla@gmail.com>
2026-02-07 19:53:36 +0400

    done

---
+++ Test1.Domain/Common/BaseAuditableEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1.Domain.Common
+{
+    public abstract class BaseAuditableEntity : BaseEntity
+    {
+        public string? CreatedBy { get; set;  } 
+        public string? UpdatedBy { get; set;  } 
+        public string? DeletedBy { get; set;  }     
+    }
+}
---
+++ Test1.Domain/Enums/Enums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1.Domain.Enums
+{
+    public enum BookingStatus
+    {
+       Pending = 0,
+       Confirmed = 1,
+       InProgress = 2,
+       Completed = 3,
+       Cancelled = 4,
+       Rejeted = 5,
+    }
+}
